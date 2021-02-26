410f4f36390206c886ebe04adac98923cf46444a
Ruslan Tuchkov <rtuchkov@skbkontur.ru>
2021-02-26 21:30:32 +0500

    lazy loading

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {lazy, Suspense} from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
-import MarketingApp from './components/MarketingApp'
-import AuthApp from './components/AuthApp'
 import Header from './components/Header'
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'))
+const AuthLazy = lazy(() => import('./components/AuthApp'))
 
 const generateClassName = createGenerateClassName({
   // задаем уникальный для этого приложение префикс для классов в продакшене,
@@ -19,10 +20,12 @@ export default () => {
       <BrowserRouter>
         <div>
           <Header/>
-          <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </BrowserRouter>
     </StylesProvider>
